a1562e6aa1d5c98ff39ae64f124bb5262e9035ce
Monmcgt <71365435+Monmcgt@users.noreply.github.com>
2022-08-29 22:36:49 +0700

    Initial Commit

--- src/main/java/me/monmcgt/code/onstance/library/java/OnstanceConnector.java
+++ src/main/java/me/monmcgt/code/onstance/library/java/OnstanceConnector.java
@@ -1,2 +1,27 @@
-package me.monmcgt.code.onstance.library.java;public class OnstanceConnector {
+package me.monmcgt.code.onstance.library.java;
+
+import me.monmcgt.code.onstance.library.java.object.OnstanceResponse;
+import me.monmcgt.code.onstance.library.java.socket.SocketClient;
+
+import java.util.function.Consumer;
+
+public class OnstanceConnector {
+    private final int port;
+    private final String uid;
+    private final Consumer<OnstanceResponse> consumer;
+
+    public OnstanceConnector(String uid, Consumer<OnstanceResponse> consumer) {
+        this(56780, uid, consumer);
+    }
+
+    public OnstanceConnector(int port, String uid, Consumer<OnstanceResponse> consumer) {
+        this.port = port;
+        this.uid = uid;
+        this.consumer = consumer;
+    }
+
+    public void start() {
+        SocketClient socketClient = new SocketClient(this.port, this.uid, this.consumer);
+        socketClient.start();
+    }
 }
--- src/main/java/me/monmcgt/code/onstance/library/java/socket/SocketClient.java
+++ src/main/java/me/monmcgt/code/onstance/library/java/socket/SocketClient.java
@@ -1,2 +1,40 @@
-package me.monmcgt.code.onstance.library.java.socket;public class SocketClient {
+package me.monmcgt.code.onstance.library.java.socket;
+
+import me.monmcgt.code.onstance.library.java.object.OnstanceResponse;
+
+import java.net.Socket;
+import java.util.function.Consumer;
+
+public class SocketClient extends Thread {
+    private final Socket socket;
+    private final String uid;
+    private final Consumer<OnstanceResponse> consumer;
+
+    public SocketClient(int port, String uid, Consumer<OnstanceResponse> consumer) {
+        this("localhost", uid, port, consumer);
+    }
+
+    public SocketClient(String ip, String uid, int port, Consumer<OnstanceResponse> consumer) {
+        try {
+            this.socket = new Socket(ip, port);
+            this.uid = uid;
+            this.consumer = consumer;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        SocketHandler socketHandler = new SocketHandler(this.socket, this.uid, this.consumer);
+        socketHandler.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                this.socket.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }));
+    }
 }
