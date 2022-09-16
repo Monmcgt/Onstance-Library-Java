05804759700db5d351d3c30905f862fc1bae23a4
Monmcgt <raninmon2015@gmail.com>
2022-09-17 02:44:46 +0700

    update TestMain.java

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -3,10 +3,11 @@ import me.monmcgt.code.onstance.library.java.OnstanceConnector;
 public class TestMain {
     public static void main(String[] args) {
         String uid = System.getProperty("onstance.uid");
+        String port = System.getProperty("onstance.port");
         if (uid == null) {
             uid = "555555555555555555555555555555555555555555555555";
         }
-        OnstanceConnector onstanceConnector = new OnstanceConnector(56790, uid, (response) -> {
+        OnstanceConnector onstanceConnector = new OnstanceConnector(port != null ? Integer.parseInt(port) : 56780, uid, (response) -> {
             System.out.println(response.toString());
         });
         onstanceConnector.start();
